29376d17da45bb23525f13304fca291401fb6864
vishalm89 <97724704+vishalm89@users.noreply.github.com>
2022-05-09 03:33:13 +0530

    Initial Push for CS coding Test

--- CSTest/Constants.cs
+++ CSTest/Constants.cs
@@ -6,10 +6,10 @@ namespace CSTest
     {
         public static List<CategoryRule> ruleList = new List<CategoryRule>(new[]
         {
+            // Here you can add, modify and remove any rule
             new CategoryRule(){riskType="LOWRISK" , operatorType= "Less Than",threshold= 1000000,sector= "Public" },
             new CategoryRule(){riskType="MEDIUMRISK" , operatorType= "Greater Than",threshold= 1000000,sector= "Public" },
             new CategoryRule(){riskType="HIGHRISK" , operatorType= "Greater Than",threshold= 1000000,sector= "Private" },
-             new CategoryRule(){riskType="LOWESTRisk" , operatorType= "Less Than",threshold= 1000,sector= "Private" },
         });
 
     }
